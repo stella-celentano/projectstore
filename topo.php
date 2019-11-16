04e61c4f1ce5dff98b1795f40dcc33d62b3c6d3b
stella-celentano <stellacelentano99@gmail.com>
2019-11-16 18:58:14 -0300

    alterações topo

--- topo.php
+++ topo.php
@@ -14,29 +14,32 @@ require_once "painel/usuarios/conexao.php";
     <link rel="shortcut icon" type="image/x-png" href="img/logo.png">
     <link rel="stylesheet" type="text/css" href="bootstrap/css/bootstrap.css">
     <link rel="stylesheet" type="text/css" href="css/estilo.css">
-    <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet"> 
-    <link href="https://fonts.googleapis.com/css?family=EB+Garamond:600&display=swap" rel="stylesheet">  
+    <link href="https://fonts.googleapis.com/css?family=Lobster&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=EB+Garamond:600&display=swap" rel="stylesheet">
 </head>
 
 <body>
-    <nav class="navbar navbar-expand-lg navbar-light ">
-        <img src="img/logo.png" alt="logo" id="logo">
-        <a class="navbar-brand" href="#" id="name">Reaproveite</a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
-            <div class="navbar-nav">
-                <a class="nav-item nav-link" href="#">Novidades</a>
-                <a class="nav-item nav-link" href="#">Promoções</a>
-                <a class="nav-item nav-link" href="#">Feminino</a>
-                <a class="nav-item nav-link" href="#">Masculino</a>
-                <a class="nav-item nav-link" href="#">Infantil</a>
+    <div class="nav">
+        <nav class="navbar navbar-expand-lg navbar-light">
+            <img src="img/logo.png" alt="logo" id="logo">
+            <a class="navbar-brand" href="#" id="name">Reaproveite</a>
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-item nav-link" href="#">Novidades</a>
+                    <a class="nav-item nav-link" href="#">Promoções</a>
+                    <a class="nav-item nav-link" href="#">Feminino</a>
+                    <a class="nav-item nav-link" href="#">Masculino</a>
+                    <a class="nav-item nav-link" href="#">Infantil</a>
+                </div>
             </div>
-        </div>
-        <div>
-            <a class="nav-item nav-link" id="btnLogin" href="#"><b>Entrar/Cadastrar</b></a>
-        </div>
-    </nav>
+            <div>
+                <a class="nav-item nav-link" id="btnLogin" href="#"><b>Entrar/Cadastrar</b></a>
+            </div>
+        </nav>
+    </div>
+
 </body>
 
 </html>
